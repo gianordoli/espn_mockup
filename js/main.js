dccc81700c9140408219981d76848d9f1969ad71
gianordoli <gianordoligabriel@yahoo.com.br>
2014-12-02 11:49:02 -0500

    Implemented sidebar animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
 
+	$('body').bind('mouseup', function(event){
+		var right = ($(window).width() - ($('#menu_widgets').offset().left + $('#menu_widgets').width()));
+		console.log(right);
+		if(right == 0){
+			$('#menu_widgets').animate({right: -$('#menu_widgets').width()}, 500, 'swing');		
+		}else{
+			$('#menu_widgets').animate({right: 0}, 500);		
+		}
+
+	});
  
 });
