38fb64e289490b246b383825d0f53e2c8fe5cb38
gianordoli <gianordoligabriel@yahoo.com.br>
2014-12-02 12:27:59 -0500

    hamburger working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
-	$('body').bind('mouseup', function(event){
+	$('#hamburger').bind('mouseup', function(event){
 		var right = ($(window).width() - ($('#menu_widgets').offset().left + $('#menu_widgets').width()));
 		console.log(right);
 		if(right == 0){
-			$('#menu_widgets').animate({right: -$('#menu_widgets').width()}, 500, 'swing');		
+			$('#menu_widgets').animate({right: -$('#menu_widgets').width() + $('#hamburger').width()}, 500, 'swing');		
 		}else{
 			$('#menu_widgets').animate({right: 0}, 500);		
 		}
