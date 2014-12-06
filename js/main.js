61370f2d9b5773e8e27950e100713605063e4224
gianordoli <gianordoligabriel@yahoo.com.br>
2014-12-05 19:58:01 -0500

    Made the function generic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+
+	// CSS navigation
     $('a.button').bind('mouseup', function(event){
     	var widgets = $('a.button');
     	$.each(widgets, function(){
@@ -8,6 +10,8 @@ $(document).ready(function () {
         $(this).addClass('selected');
     });
 
+
+    // Sidebar
 	$('#hamburger').bind('mouseup', function(event){
 		var right = ($(window).width() - ($('#menu_widgets').offset().left + $('#menu_widgets').width()));
 		console.log(right);
@@ -19,32 +23,37 @@ $(document).ready(function () {
 	});
 
 
+	// Sidebar widgets
 	$('#channels').bind('mouseup', function(event){
+		var myId = $(this).attr('id');
+		var img_index = 0;
 
-		var channelsIndex = 0;
-
-    	var myImg = $('<img src="img/channels_' + channelsIndex + '.jpg"/>')
+    	var myImg = $('<img src="img/' + myId + '_' + img_index + '.jpg"/>')
     				.bind('mouseup', function(){
 
-    					// increase step	
-    					channelsIndex = increaseSteps(channelsIndex, 1, function(i){
+    					// increase step
+    					// (image_index, max #steps before callback, and callback)
+    					img_index = increaseSteps(img_index, 1, function(i){
 
     						// Creates image
-    						var windowImg = $('<img src="img/channels_' + i + '.jpg"/>');
+    						var windowImg = $('<img src="img/' + myId + '_' + i + '.jpg"/>');
     						
-    						// Creates window
-    						createDraggableWidget(100, 200, 400, 400, 'channels_widget', windowImg);
-    					});    					
-    					$(this).attr('src', 'img/channels_' + channelsIndex + '.jpg');
+    						// Creates window (x, y, w, h, id, img)
+    						createDraggableWidget(100, 200, 400, 400, myId + '_widget', windowImg);
+    					});
+
+    					//Replaces the image with the next one
+    					$(this).attr('src', 'img/' + myId + '_' + img_index + '.jpg');
     				});
 
+    	// Replaces the content of the sidebar with the new img
         $('#menu_widgets_content').html(myImg);
 	});	
 
 	function createDraggableWidget(x, y, w, h, id, img){
-		// If the window hasn't been created yet...
-		if($(id).length == 0){
-			var newWidget = $('<div class="draggable"></div>')
+		// If a window with this id hasn't been created yet...
+		if($('#'+id).length == 0){
+			var newWidget = $('<div id=' + id + ' class="draggable"></div>')
 							.css({
 								'position': 'absolute',
 								'top': x,
