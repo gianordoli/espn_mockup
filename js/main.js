86dd8790d1ab9b588a1e582b669eb1ce18236c8d
gianordoli <gianordoligabriel@yahoo.com.br>
2014-12-06 19:31:04 -0500

    Draggable channel window working

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 		var myId = $(this).attr('id');
 		var img_index = 0;
 
-    	var myImg = $('<img src="img/' + myId + '_' + img_index + '.jpg"/>')
+    	var myImg = $('<img src="img/' + myId + '_' + img_index + '.svg"/>')
     				.bind('mouseup', function(){
 
     					// increase step
@@ -39,35 +39,94 @@ $(document).ready(function () {
     					img_index = increaseSteps(img_index, 1, function(i){
 
     						// Creates image
-    						var windowImg = $('<img src="img/' + myId + '_' + i + '.jpg"/>');
+    						var windowImg = $('<img src="img/' + myId + '_' + i + '.svg"/>');
     						
     						// Creates window (x, y, w, h, id, img)
-    						createDraggableWidget(100, 200, 400, 400, myId + '_widget', windowImg);
+    						createDraggableWidget(800, 400, myId + '_widget', windowImg);
     					});
 
     					//Replaces the image with the next one
-    					$(this).attr('src', 'img/' + myId + '_' + img_index + '.jpg');
+    					$(this).attr('src', 'img/' + myId + '_' + img_index + '.svg');
     				});
 
     	// Replaces the content of the sidebar with the new img
         $('#menu_widgets_content').html(myImg);
 	});	
 
-	function createDraggableWidget(x, y, w, h, id, img){
+	// Chat widgets
+	var chat_index = 0;	
+	$('#chat').bind('mousedrag', function(event){
+		var myId = $(this).attr('id');
+		chat_index = increaseSteps(chat_index, 8, function(i){});
+		var myImg = $('<img src="img/' + myId + '_' + chat_index + '.svg"/>');
+        $(this).html(myImg);
+	});	
+
+	var draggedObj = '';
+	var dragOffset;
+
+	function createDraggableWidget(x, y, id, img){
 		// If a window with this id hasn't been created yet...
 		if($('#'+id).length == 0){
 			var newWidget = $('<div id=' + id + ' class="draggable"></div>')
 							.css({
 								'position': 'absolute',
-								'top': x,
-								'left': y,
-								'width': w,
-								'height': h
+								'left': x,
+								'top': y
 							});
 			$(newWidget).append(img);
+			$(newWidget).bind('mousedown', function(event){
+				event.preventDefault();
+				// console.log('x: ' + $(this).offset().left + ', y: ' + $(this).offset().top);
+				// console.log('x: ' + event.pageX + ', y: ' + event.pageY);
+				dragOffset = {
+					x: event.pageX - $(this).offset().left,
+					y: event.pageY - $(this).offset().top
+				}	
+
+				// Close
+				if(210 < dragOffset.x && dragOffset.x < $(this).width() &&
+				   0 < dragOffset.y && dragOffset.y < 80){
+					// console.log('close');
+					$(this).remove();
+
+				// Start dragging
+				}else if(0 < dragOffset.y && dragOffset.y < 40){
+					draggedObj = this;
+				}
+			});
+			$(newWidget).bind('mouseup', function(event){
+				// console.log($(this).attr('id'));
+				if($(draggedObj).attr('id') == $(this).attr('id')){
+					draggedObj = '';	
+				}else{
+					var currSrc = $(this).children('img').attr('src');
+					var currIndex = currSrc.charAt(currSrc.lastIndexOf('_') + 1);
+					console.log(currIndex);
+					var newSrc = currSrc;
+					if(currIndex == '2'){
+						// console.log('hey');
+						newSrc = currSrc.replace('2', '3');
+					}else if(currIndex == '3'){
+						newSrc = currSrc.replace('3', '2');
+					}
+					console.log(newSrc);
+					console.log($(this).children('img').attr('src', newSrc));
+				}
+			});			
+
 			$('body').append(newWidget);			
 		}
 	}
+
+	$('body').bind('mousemove', function(event){
+		if(draggedObj != ''){
+			$(draggedObj).css({
+				'left': event.pageX - dragOffset.x,
+				'top': event.pageY - dragOffset.y
+			});			
+		}
+	});
 
 	function increaseSteps(index, max, callback){
 		index ++;
